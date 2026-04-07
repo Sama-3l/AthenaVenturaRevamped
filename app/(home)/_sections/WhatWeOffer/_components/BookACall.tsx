d3d4e8becfed2f5ee936ecc7593ebc4c277e1a1c
Raghvendra Mishra <raghvendramishra2002@gmail.com>
2026-04-07 21:00:12 +0530

    Button update

--- app/(home)/_sections/WhatWeOffer/_components/BookACall.tsx
+++ app/(home)/_sections/WhatWeOffer/_components/BookACall.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function BookACall({ bgColor = 'var(--color-background)', textColor = 'var(--color-foreground)' }: { bgColor?: string, textColor?: string }) {
   return (
     <div
-      className='flex flex-col font-(family-name:--font-fustat) tracking-[-5%] text-[16px] w-full py-3 items-center rounded-full font-bold'
+      className='flex flex-col font-(family-name:--font-fustat) tracking-[-5%] text-[16px] w-full py-3 items-center rounded-full font-bold cursor-pointer'
       style={{ backgroundColor: bgColor, color: textColor }}
     >
       Book a call
