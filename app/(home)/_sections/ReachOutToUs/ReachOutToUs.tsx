9c16a2e502b0a4152098b7f3447c17bceeb3123f
Raghvendra Mishra <raghvendramishra2002@gmail.com>
2026-04-10 01:35:56 +0530

    Part of same

--- app/(home)/_sections/ReachOutToUs/ReachOutToUs.tsx
+++ app/(home)/_sections/ReachOutToUs/ReachOutToUs.tsx
@@ -37,7 +37,7 @@ export default function ReachOutToUs() {
                         Contact Us
                     </h2>
                 </div>
-                <div className='flex flex-col gap-1 px-8 w-[70%] md:w-[90%]'>
+                <div className='flex flex-col gap-1 px-8 w-[70%] md:w-[90%] lg:w-[70%]'>
                     <p className={styles.page_body}>
                     We are a small creative studio-cum-software development firm dedicated to launch of new ventures and transforming established brands. We curate captivating designs that lead our teams to bring your ideas and visions to life.<br /><br />Building brands, apps and websites our team is here to help you start, grow or elevate your enterprise.
                     </p>
