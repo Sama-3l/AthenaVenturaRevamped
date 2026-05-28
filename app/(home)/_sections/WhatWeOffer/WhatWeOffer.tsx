00c69831d89b9b1431c9664c596e57dfe748f2c6
Raghvendra Mishra <raghvendramishra2002@gmail.com>
2026-05-28 21:59:24 +0530

    Pricing update

--- app/(home)/_sections/WhatWeOffer/WhatWeOffer.tsx
+++ app/(home)/_sections/WhatWeOffer/WhatWeOffer.tsx
@@ -44,7 +44,7 @@ export default function WhatWeOffer() {
             "CMS / blog",
             "Advanced SEO",
             "Extra pages"
-          ]} amount={'$499'} />
+          ]} amount={'$699'} />
             <PackageCard title="Foundation" deadline={'8-12 weeks'} description={'An app to rule them all.'} core={{
             "Core": [
               "Everything in Pixel core",
